f25b87f70116f2cfbd69beefa9c01b152ebd7735
Ivan Stoyanov <ivanstoyanov@outlook.com>
2024-09-03 11:34:15 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col font-matemasie text-green-500 text-5xl justify-center items-center h-screen bg-rose-500 gap-10">
+        <h1 className="">JJ Hings</h1>
+        <h2>Ice Cream and Things</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      //fontFamily: { jacquard: "Jacquard 12 Charted", montserrat: "Montserrat" },
+      fontFamily: { matemasie: "Matemasie" },
+    },
+  },
+  plugins: [],
+};
